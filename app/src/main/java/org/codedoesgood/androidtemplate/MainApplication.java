dafd31d04c4af269a87dd5bb0cb02650f08360f9
Brandon <brandonpas22@gmail.com>
2017-05-13 17:56:04 -0400

    Add Timber configuration for debug. Add checkstyle task on assembleDebug

---
+++ app/src/main/java/org/codedoesgood/androidtemplate/MainApplication.java
@@ -0,0 +1,19 @@
+package org.codedoesgood.androidtemplate;
+
+import android.app.Application;
+import timber.log.Timber;
+
+/**
+ * Default launch activity for template
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+}
