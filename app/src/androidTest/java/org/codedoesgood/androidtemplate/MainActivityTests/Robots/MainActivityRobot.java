c9633dfef77bc66b21415ee964b4c84b79fb6676
Brandon <brandonpas22@gmail.com>
2017-06-26 21:10:51 -0400

    Fix comment alignment

--- app/src/androidTest/java/org/codedoesgood/androidtemplate/MainActivityTests/Robots/MainActivityRobot.java
+++ app/src/androidTest/java/org/codedoesgood/androidtemplate/MainActivityTests/Robots/MainActivityRobot.java
@@ -5,7 +5,7 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-/**
+ /**
   * The robot class should contain high-level actions that a standard QA team member
   * would perform. It should contain logic for how a value or an action would be taken,
   * but not specific values.
